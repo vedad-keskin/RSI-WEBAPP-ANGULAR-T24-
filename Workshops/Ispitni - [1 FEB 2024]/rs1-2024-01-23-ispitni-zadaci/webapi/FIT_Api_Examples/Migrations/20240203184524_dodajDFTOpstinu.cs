bded7571cf34d2d489d0b94a7984dc8308a6a1e4
Vedad <vedad.ribery@gmail.com>
2024-02-03 22:04:46 +0100

    Dodavanje prve polovine 1FEB 24

---
+++ Workshops/Ispitni - [1 FEB 2024]/rs1-2024-01-23-ispitni-zadaci/webapi/FIT_Api_Examples/Migrations/20240203184524_dodajDFTOpstinu.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FIT_Api_Examples.Migrations
+{
+    public partial class dodajDFTOpstinu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DefaultnaOpstinaId",
+                table: "KorisnickiNalog",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KorisnickiNalog_DefaultnaOpstinaId",
+                table: "KorisnickiNalog",
+                column: "DefaultnaOpstinaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_KorisnickiNalog_Opstina_DefaultnaOpstinaId",
+                table: "KorisnickiNalog",
+                column: "DefaultnaOpstinaId",
+                principalTable: "Opstina",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_KorisnickiNalog_Opstina_DefaultnaOpstinaId",
+                table: "KorisnickiNalog");
+
+            migrationBuilder.DropIndex(
+                name: "IX_KorisnickiNalog_DefaultnaOpstinaId",
+                table: "KorisnickiNalog");
+
+            migrationBuilder.DropColumn(
+                name: "DefaultnaOpstinaId",
+                table: "KorisnickiNalog");
+        }
+    }
+}
